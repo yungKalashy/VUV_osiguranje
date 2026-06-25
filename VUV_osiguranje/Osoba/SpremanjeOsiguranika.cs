2ef781bda2b7de0610cacd8a80a0226c03284fb3
Daniel <daniel.malenjak@gmail.com>
2026-06-25 19:27:57 +0200

    V5

--- VUV_osiguranje/Osoba/SpremanjeOsiguranika.cs
+++ VUV_osiguranje/Osoba/SpremanjeOsiguranika.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
-using System.Xml.Linq;
 
 namespace VUV_osiguranje.Osoba
 {
